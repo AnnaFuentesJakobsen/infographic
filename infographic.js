fbd82087f5067cba475d66e100c4d455250921af
Anna Fuentes Jakobsen <afuentesjakobsen@Annas-MacBook-Pro-2.local>
2016-08-30 21:15:00 +0200

    added weather api and content to page

---
+++ infographic.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$("#weatherBtn").click(function) {
+		$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Tokyo&APPID=f34fe67934cfb321117737abf83379e0",   
+
+	});
+});
