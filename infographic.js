dd4998fa919eb06a9f97ccb71621a2d81d8c85c5
Anna Fuentes Jakobsen <afuentesjakobsen@Annas-MacBook-Pro-2.local>
2016-08-31 11:27:45 +0200

    added weather function

--- infographic.js
+++ infographic.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-	$("#weatherBtn").click(function) {
-		$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Tokyo&APPID=f34fe67934cfb321117737abf83379e0",   
+	$("#weatherbtn").click(function() {
+		$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Tokyo&APPID=f34fe67934cfb321117737abf83379e0', 
+			function(wRes) { 
+			//document.write(wRes.id); 
+				$.each(wRes.weather, function(i, weather) {
+					var tempeture = "<div>";
+					tempeture += "<p>" + weather.id + "</p>";
+					tempeture += "</div>";
 
-	});
+					$("#weatherLog").append(tempeture);
+
+					console.log(tempeture);
+				});
+			});
+		});
 });
