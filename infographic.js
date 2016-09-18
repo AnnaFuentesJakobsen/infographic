e71652d54a7eebf8b2eedb28f848212f6b541ca8
Anna Fuentes Jakobsen <afuentesjakobsen@Annas-MacBook-Pro-2.local>
2016-09-18 20:57:33 +0200

    added flag img and sake img, added some new functions

--- infographic.js
+++ infographic.js
@@ -1,36 +1,41 @@
-$(document).ready(function(){
-	$("button").click(function(){
-		$(".basicFacts").toggle(),
-		$(".hide-this").toggle();
-	});
-});
-$(window).load(function() {
+// Load
+/*$(window).load(function() {
   $("main").fadeIn(2000);
 });
+*/
 
+$( "h1" ).hover(
+  function() {
+    $( this ).append( $( "<span> ^___^</span>" ) );
+  }, function() {
+    $( this ).find( "span:last" ).remove();
+  }
+);
 
-
-$(document).ready(function() {
-	$("#weatherbtn").click(function() {
-		$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Tokyo&APPID=f34fe67934cfb321117737abf83379e0', 
-			function(wRes) { 
-			//document.write(wRes.id); 
-				$.each(wRes.weather, function(i, weather) {
-					var tempeture = "<div>";
-					tempeture += "<p>" + weather.id + "</p>" + "<br>";
-					tempeture += "<p>" + weather.main + "</p>" + "<br>";
-					/*pics += "<em>" + item.author + "</em>" + "<br>";
-					pics += "<p>" + item.date_taken + "</p>" + "<br>";
-					pics += "<button id='" + i + "' author-id='" + item.author_id + "'>" + "Users favorites" + "</button>";
-					*/
-					tempeture += "</div>";
-
-					$("#weatherLog").append(tempeture);
-
-					console.log(tempeture);
-				});
-			});
-		});
+$(document).ready(function(){
+	$(".map-btn").click(function(){
+		$(".map").fadeIn(2000);
+	});
 });
 
+// Add Class
+
+$(".map-btn").click(function(){
+    $(".map-btn").addClass("hide-button");
+});
+
+
+// Toggle
+
+$(document).ready(function(){
+	$(".etiquette-btn").click(function(){
+		$(".etiquette").toggle();
+	});
+});
+
+$(document).ready(function(){
+	$(".random-btn").click(function(){
+		$(".random").toggle();
+	});
+});
 
